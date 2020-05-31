1b0b77a1b4d1f0d0b57b79d78d3af30b63b23f14
satelitcellular <64521084+satelitcellular@users.noreply.github.com>
2020-05-31 23:42:25 +0800

    Updated from Publii

--- app/back-end/app-preload.js
+++
@@ -1,8 +0,0 @@
-const ContextMenuBuilder = require('./helpers/context-menu-builder.js');
-const ContextMenuListener = require('./helpers/context-menu-listener.js');
-
-let contextMenuBuilder = new ContextMenuBuilder();
-let contextMenuListener = new ContextMenuListener((info) => {
-    contextMenuBuilder.showPopupMenu(info);
-});
-
--- app/src/components/mixins/BackToTools.js
+++
@@ -1,8 +0,0 @@
-export default {
-    methods: {
-        goBack: function() {
-            let siteName = this.$route.params.name;
-            this.$router.push('/site/' + siteName + '/tools/');
-        }
-    }
-};
